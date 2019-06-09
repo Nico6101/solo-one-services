47dfdfcaa03610e733fd4626df9046fdb9763af6
Nico6101 <singhnitesh3712@gmail.com>
2019-06-09 19:43:29 +0530

    modified login page

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'dotenv/config'
 import Login from './loginpage'
+
 class App extends React.Component {
     constructor(props) {
         super(props);
--- src/components/loginpage.js
+++ src/components/loginpage.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import '../login.css'
+import loginScss from './LoginPage.scss'
+import env from '../env'
 import FacebookLogin from 'react-facebook-login'
 import { Card, CardBody, CardTitle, Col, Row } from 'reactstrap'
-
 class LoginPage extends React.Component {
     render() {
         const responseFacebook = (response) => {
             console.log(response);
         }
         return (
-            <Row>
-                <Col sm="6">
-                    <div className="row">
-                        <h1>login Page</h1>
-                        <Card body className="text-center" style={{ backgroundColor: "white", backgroundPosition: "center" }}>
+            <Row style={{ textAlign: "center" }}>
+                <Col style={{ Tmargin: "10px" }}>
+                    <div >
+                        {/* <h1>login Page</h1> */}
+                        <Card body className = "cardbody">
                             <CardBody>
-                                <CardTitle>LOGIN</CardTitle>
-                                <div className="col rs1 bs1">
-                                    <FacebookLogin appId="354777901798167" fields="name,email,picture" callback={responseFacebook} />
-                                </div>
+                                <CardTitle style={{ backgroundColor: "white", margin: "50px", fontWeight: "bold", fontSize: "40px", color: "blue" }}>LOGIN</CardTitle>
+                                <button className="fbbutton">
+                                    <FacebookLogin appId={env.APPID} fields="name,email,picture" callback={responseFacebook} />
+                                </button>
                             </CardBody>
                         </Card>
                     </div>
@@ -28,4 +29,7 @@ class LoginPage extends React.Component {
     }
 }
 
+Card.propTypes = {
+    className: "card-body"
+}
 export default LoginPage;
